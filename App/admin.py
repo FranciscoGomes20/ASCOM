5e2c6467b1bb5a93257e7642bf8a138f862c063f
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-01-11 11:02:12 -0500

    A APP e as models foram iniciadas.

---
+++ App/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Status)
+admin.site.register(Designer)
+admin.site.register(Urgency)
+admin.site.register(Sector)
+admin.site.register(Requestion)
---
+++ App/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Status(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Designer(models.Model):
+    name = models.CharField(max_length=150)
+    password = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+class Urgency(models.Model):
+    description = models.CharField(max_length=150)
+
+    def __str__(self) -> str:
+        return self.description
+
+class Sector(models.Model):
+    sector = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.sector
+    
+class Requestion(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    anexo = models.ImageField('Imagem', upload_to='App\img_upload')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, blank=True, null=True)
+    applicant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    assigned = models.ForeignKey(Designer, on_delete=models.CASCADE, blank=True, null=True)
+    urgency = models.ForeignKey(Urgency, on_delete=models.CASCADE, blank=True, null=True)
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE, blank=True, null=True)
+    published_date = models.DateTimeField(default=timezone.now)
+    final_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.title
