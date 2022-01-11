3f243a68cca3806dab788d6fc9b0062a712b70be
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-01-11 11:20:49 -0500

    Personalizando o django admin.

--- App/admin.py
+++ App/admin.py
@@ -3,8 +3,17 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Category)
+
 admin.site.register(Status)
+
 admin.site.register(Designer)
+
 admin.site.register(Urgency)
+
 admin.site.register(Sector)
-admin.site.register(Requestion)
+
+@admin.register(Requestion)
+class RequestionAdmin(admin.ModelAdmin):
+    list_display = ("title", "applicant", "status", "final_date")
+    list_filter = ("applicant", "status", )
+    search_fields = ("title", )
