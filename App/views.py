77bbc16b45709959207a1b199c88776a603c908c
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-01-13 10:48:16 -0500

    Adicionando o static no html

--- App/views.py
+++ App/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, "app\login.html")
+
+def index(request):
+    return render(request, "app\index.html")
