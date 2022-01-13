6472abf8ba993d54bf6ec4d003df2ffdb1a15638
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-01-13 17:18:25 -0500

    Autenticação de usuario.

--- App/urls.py
+++ App/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.login_request, name='login'),
+    path('logout', views.logout_request, name='logout'),
     path('index', views.index, name='index'),
 ]
