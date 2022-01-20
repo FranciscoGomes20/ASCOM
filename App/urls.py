a3f9ff1b458cb087620b56f5b6b9afd98bdcacfa
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-01-20 11:35:53 -0500

    O erro no templatetags foi corrigido

--- App/urls.py
+++ App/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login', views.login_request, name='login'),
-    path('logout', views.logout_request, name='logout'),
     path('', views.index, name='index'),
 ]
--- App/views.py
+++ App/views.py
@@ -1,33 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate, logout
-from django.contrib import messages
 
 # Create your views here.
 def login_request(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.info(request, f"Você está logado agora {username}.")
-                return redirect("index")
-            else:
-                messages.error(request, "Username or password is invalid.")
-        else:
-            messages.error(request,"Invalid username or password.")
-    form = AuthenticationForm()
-    return render(request, "registration/login.html", {'form':form})
+    return render(request, "registration/login.html")
 
 @login_required
 def index(request):
     return render(request, "app/index.html")
-
-def logout_request(request):
-    logout(request)
-    messages.info(request, "You have successfully logged out.")
-    redirect('login_request')
